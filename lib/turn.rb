c8f8e9524c219f2ebed28174f247b12c762555e2
Luke Sherwood <luke.sherwood55@gmail.com>
2019-12-10 06:52:54 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -44,7 +44,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index) == true
-    move(board, index, character)
+    move(board, index, character="X")
   else 
     puts "Please enter a valid number"
     turn(board)
